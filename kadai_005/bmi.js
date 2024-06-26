d898d7b387636430c25cd61995393f521eecd1cf
YasukoKitano <yasuko.uexy.kitano@gmail.com>
2024-06-26 17:51:10 +0900

    kadai bmi re-submit

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,15 +1,15 @@
+//変数の代入
+let bodyWeight = 68;
+
+// 変数の代入
+let height = 1.7;
+
 // 変数の宣言
-let bodyWeight;
+ let BMI;
 
-// 値の代入
-bodyWeight = '68';
-
-// 変数の宣言
-let heigh;
-
-// 値の代入
-heigh = '1.7';
+ // 値の代入
+ BMI = ((bodyWeight)/(height * height));
 
 // コンソールへの出力
-console.log((bodyWeight) / (heigh * heigh));
+console.log(BMI);
 
